1fd3a67d9862feae02ca86bf279f612e51f41c61
Erwan Lemonnier <erwan@lemonnier.se>
2017-10-17 06:53:19 +0200

    Silence logs from boto3

--- klue_microservice/log.py
+++ klue_microservice/log.py
@@ -13,8 +13,9 @@ root.setLevel(DEFAULT_LEVEL)
 
 # Make an exception for boto and bravado: its debug level is just too verbose...
 logging.getLogger('boto').setLevel(logging.INFO)
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
 logging.getLogger('bravado_core.spec').setLevel(logging.INFO)
-logging.getLogger('amqp').setLevel(logging.INFO)
 logging.getLogger('swagger_spec_validator.ref_validators').setLevel(logging.INFO)
 
 
