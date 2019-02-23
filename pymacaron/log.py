1fcfd71c3cd717837575f3d555679ccf4a3ba169
Erwan Lemonnier <erwan@lemonnier.se>
2019-02-23 10:25:42 +0100

    Put logger setup in a method, also called when starting celery worker

--- pymacaron/log.py
+++ pymacaron/log.py
@@ -4,21 +4,31 @@ import sys
 DEFAULT_LEVEL = logging.DEBUG
 
 root = logging.getLogger()
-ch = logging.StreamHandler(sys.stdout)
 
-formatter = logging.Formatter('%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root.addHandler(ch)
-root.setLevel(DEFAULT_LEVEL)
+def setup_logger(celery=False):
+    global root
 
-# Make an exception for boto and bravado: its debug level is just too verbose...
-logging.getLogger('boto').setLevel(logging.INFO)
-logging.getLogger('boto3').setLevel(logging.WARNING)
-logging.getLogger('botocore').setLevel(logging.WARNING)
-logging.getLogger('bravado_core.spec').setLevel(logging.INFO)
-logging.getLogger('swagger_spec_validator.ref_validators').setLevel(logging.INFO)
-logging.getLogger('celery').setLevel(logging.INFO)
+    # Celery setups a default handler: remove it
+    root.handlers = []
 
+    ch = logging.StreamHandler(sys.stdout)
+
+    name = 'WORKER' if celery else 'FLASK'
+    formatter = logging.Formatter('%(asctime)s - ' + name + ' %(process)d - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    root.setLevel(DEFAULT_LEVEL)
+
+    # Make an exception for boto and bravado: its debug level is just too verbose...
+    logging.getLogger('boto').setLevel(logging.INFO)
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('botocore').setLevel(logging.WARNING)
+    logging.getLogger('bravado_core.spec').setLevel(logging.INFO)
+    logging.getLogger('swagger_spec_validator.ref_validators').setLevel(logging.INFO)
+    logging.getLogger('celery').setLevel(logging.INFO)
+    logging.getLogger('scout_apm.core').setLevel(logging.INFO)
+
+setup_logger()
 
 def get_logger():
     global root
