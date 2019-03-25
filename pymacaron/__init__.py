99b10ddf74d8d556177fced49f8af75c78057157
Erwan Lemonnier <erwan@lemonnier.se>
2019-03-25 16:30:37 +0100

    Default to starting only 1 celery worker when running local

--- pymacaron/__init__.py
+++ pymacaron/__init__.py
@@ -310,7 +310,7 @@ def letsgo(name, callback=None):
         # Start celeryd and redis?
         if with_async:
             from pymacaron_async import start_celery
-            start_celery(port, debug)
+            start_celery(port, debug, concurrency=1)
 
         # Proceed to start the API server
         callback(port, debug)
