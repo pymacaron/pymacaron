cf3ea57d0fb7015149bc84645f824583f09c6b85
Erwan Lemonnier <erwan@lemonnier.se>
2020-02-22 11:19:58 +0100

    Reduced the memory requirement per gunicorn worker: 600Mb instead of 900Mb

--- pymacaron/resources.py
+++ pymacaron/resources.py
@@ -35,12 +35,12 @@ def get_celery_worker_count(cpu_count=None):
 def get_memory_limit(default_celery_worker_count=None, cpu_count=None):
     """Return the memory in Megabytes required to run pymacaron on this container hardware"""
     # Let's calculate how much memory this pymacaron config requires for 1 container
-    # Each celery process takes 300Mb
-    # Each gunicorn worker takes 900Mb
+    # Each celery process takes 200Mb
+    # Each gunicorn worker takes 600Mb
     celery_count = default_celery_worker_count
     if not celery_count:
         celery_count = get_celery_worker_count(cpu_count=cpu_count)
-    return ceil(get_gunicorn_worker_count(cpu_count=cpu_count) * 900 + celery_count * 300)
+    return ceil(get_gunicorn_worker_count(cpu_count=cpu_count) * 600 + celery_count * 200)
 
 
 def get_celery_worker_memory_limit():
