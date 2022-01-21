1147a386577cbfe3d2f2b6682780f705e591c9ad
Erwan Lemonnier <erwan@lemonnier.se>
2022-01-21 12:55:06 +0100

    Alwasy allow None as a valid value for enum properties

--- pymacaron/apiloader.py
+++ pymacaron/apiloader.py
@@ -64,7 +64,7 @@ def generate_models_v2(swagger, model_file, api_name):
         elif 'enum' in prop_def:
             assert type(prop_def['enum']) is list
             str_enum = ', '.join(f'"{s}"' for s in prop_def['enum'])
-            t = f'Literal[{str_enum}]'
+            t = f'Literal[None, {str_enum}]'
             return t
         elif 'format' in prop_def:
             t = prop_def['format']
